669083f3b04ddfed9e808e5bff668f4e9451ca9c
Andrew Hoang <andrewhoang7@gmail.com>
2022-02-10 07:59:53 -0600

    code up the VideoCard component

--- VideoPlayer/VideoPlayer/Components/VideoCard.swift
+++ VideoPlayer/VideoPlayer/Components/VideoCard.swift
@@ -9,15 +9,41 @@ import SwiftUI
 
 struct VideoCard: View {
     var body: some View {
-        ZStack(alignment: .bottomLeading) {
-            AsyncImage(url: URL(string: "")) { image in
+        ZStack {
+            ZStack(alignment: .bottomLeading) {
+                AsyncImage(url: URL(string: "")) { image in
+                    image.resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width:160, height: 250)
+                        .cornerRadius(30)
+                } placeholder: {
+                    Rectangle()
+                        .foregroundColor(.gray.opacity(0.3))
+                        .frame(width: 160, height: 250)
+                        .cornerRadius(30)
+                }
                 
-            } placeholder: {
-                Rectangle()
+                VStack(alignment: .leading) {
+                    Text("32 sec")
+                        .font(.caption).bold()
+                    
+                    Text("By XYZ")
+                        .font(.caption).bold()
+                        .multilineTextAlignment(.leading)
+                }
                 
+                .foregroundColor(.white)
+                .shadow(radius: 20)
+                .padding()
             }
+            
+            Image(systemName: "play.fill")
+                .foregroundColor(.white)
+                .font(.title)
+                .padding()
+                .background(.ultraThinMaterial)
+                .cornerRadius(50)
         }
-        
     }
 }
 
