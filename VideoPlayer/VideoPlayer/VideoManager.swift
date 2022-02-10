c35697a8aa9dddfcdc436c31ecf5dc4aabca3cc4
Andrew Hoang <andrewhoang7@gmail.com>
2022-02-10 08:18:21 -0600

    creating ResponseBody based off of Pexels API
    video_pictures parameter from Pexels api not included, as well as height and widght parameters

--- VideoPlayer/VideoPlayer/VideoManager.swift
+++ VideoPlayer/VideoPlayer/VideoManager.swift
@@ -11,3 +11,34 @@ import Foundation
 enum Query: String, CaseIterable {
    case nature, animals, people, ocean, food
 }
+
+
+struct ResponseBody: Decodable {
+    let page: Int
+    let per_page: Int
+    let total_results: Int
+    let url: String
+    let videos: [Video]
+    
+    struct Video: Decodable, Identifiable {
+        let id: Int
+        let image: String
+        let duration: Int
+        let user: User
+        let video_files: [VideoFile]
+        
+        struct User: Decodable, Identifiable {
+            let id: Int
+            let name: String
+            let url: String
+        }
+        
+        struct VideoFile: Decodable {
+            let id: Int
+            let quality: String
+            let file_type: String
+            let link: String
+        }
+    }
+}
+
